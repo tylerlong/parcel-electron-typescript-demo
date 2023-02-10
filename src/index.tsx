2605ca77986f4d487f2edaf47039bc33a7e84548
Tyler Long <tyler4long@gmail.com>
2023-02-10 15:16:21 -0800

    Hello React

---
+++ src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {useProxy} from '@tylerlong/use-proxy';
+import {Component} from '@tylerlong/use-proxy/build/react';
+
+import {Button} from 'antd-mobile';
+
+class Store {
+  count = 10;
+  increase() {
+    this.count += 1;
+  }
+  decrease() {
+    this.count -= 1;
+  }
+}
+const store = useProxy(new Store());
+
+class App extends Component<{store: Store}> {
+  render() {
+    const store = this.props.store;
+    return (
+      <>
+        <Button color="primary" fill="outline" onClick={() => store.decrease()}>
+          -
+        </Button>
+        {store.count}
+        <Button color="primary" fill="outline" onClick={() => store.increase()}>
+          +
+        </Button>
+      </>
+    );
+  }
+}
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+ReactDOM.render(<App store={store} />, container);
