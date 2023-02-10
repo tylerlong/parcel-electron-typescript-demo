932eabc8470d866a0c0821838b56942d50149ecb
Tyler Long <tyler4long@gmail.com>
2023-02-10 14:30:33 -0800

    Hello electron

---
+++ src/electron.ts
@@ -0,0 +1,30 @@
+import {app, BrowserWindow} from 'electron';
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+  mainWindow.loadFile('dist/renderer/index.html');
+
+  mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
