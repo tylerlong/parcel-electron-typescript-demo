23ea23487f73a2c633d8a084e8023389b488e577
Tyler Long <tyler4long@gmail.com>
2023-02-15 13:32:05 -0800

    Try release based on metadata

---
+++ apps/demo1/metadata.ts
@@ -0,0 +1,9 @@
+import {Metadata} from 'electron-builder';
+
+const metadata: Metadata = {
+  version: '0.4.0',
+  productName: 'Demo1',
+  repository: 'github:tylerlong/parcel-electron-typescript-demo',
+};
+
+export default metadata;
--- scripts/release.ts
+++ scripts/release.ts
@@ -1,7 +1,14 @@
 import path from 'path';
 import fs from 'fs';
+import {
+  AfterPackContext,
+  CliOptions,
+  Metadata,
+  build as electronBuild,
+} from 'electron-builder';
+import {notarize} from 'electron-notarize';
 
-const release = (app: string) => {
+const release = async (app: string) => {
   fs.copyFileSync(
     path.join(__dirname, '..', 'common', 'background.png'),
     path.join(__dirname, '..', 'build', 'background.png')
@@ -10,6 +17,61 @@ const release = (app: string) => {
     path.join(__dirname, '..', 'apps', app, 'icon.png'),
     path.join(__dirname, '..', 'build', 'icon.png')
   );
+  const metadata: Metadata = await import(`../apps/${app}/metadata.ts`);
+  const options: CliOptions = {
+    x64: true,
+    arm64: true,
+    publish: 'always',
+    mac: ['default'],
+    win: ['default'],
+    config: {
+      extraMetadata: metadata,
+      appId: `${app}.chuntaoliu.com`,
+      // productName: 'Parcel Electron TypeScript Demo',
+      copyright: `Copyright © ${new Date().getFullYear()} CHUNTAO LIU`,
+      publish: [
+        {
+          provider: 'github',
+          releaseType: 'release',
+        },
+      ],
+      files: ['build'],
+      mac: {
+        category: 'public.app-category.productivity',
+      },
+      win: {
+        sign: async () => {}, // disable windows sign
+        verifyUpdateCodeSignature: false,
+      },
+      dmg: {
+        contents: [
+          {
+            x: 128,
+            y: 160,
+          },
+          {
+            x: 384,
+            y: 160,
+            type: 'link',
+            path: '/Applications',
+          },
+        ],
+      },
+      compression: 'maximum',
+      afterSign: async (context: AfterPackContext) => {
+        if (context.electronPlatformName === 'darwin') {
+          await notarize({
+            appBundleId: `${app}.chuntaoliu.com`,
+            appPath: `${context.appOutDir}/${context.packager.appInfo.productFilename}.app`,
+            appleId: process.env.APPLE_ID!,
+            appleIdPassword: process.env.APPLE_ID_PASSWORD!,
+          });
+        }
+      },
+    },
+  };
+
+  await electronBuild(options);
 };
 
 export default release;
