ca3b5af3db9dcd113721016f0f9333bfbfa7e13a
Tyler Liu <tyler4long@gmail.com>
2023-02-14 21:41:37 -0800

    Refactor code

--- scripts/cmd.ts
+++ scripts/cmd.ts
@@ -5,25 +5,6 @@ import fs from 'fs';
 import {Command} from 'commander';
 import {run} from './utils';
 
-const apps = fs
-  .readdirSync(path.join(__dirname, 'apps'))
-  .filter(name => !name.startsWith('.'));
-
-const commands = ['build', 'start', 'release'];
-
-const program = new Command();
-program
-  .option('-a --app <app>', 'app to manage')
-  .option('-c --command <command>', 'command to run');
-program.parse(process.argv);
-let {app, command} = program.opts();
-if (app && app.length > 2) {
-  app = apps.find(item => item.includes(app));
-}
-if (command && command.length > 2) {
-  command = commands.find(item => item.includes(command));
-}
-
 const build = async (app: string) => {
   const commonConfig: InitialParcelOptions = {
     defaultConfig: '@parcel/config-default',
@@ -95,9 +76,31 @@ const runCommand = async (command: string, app: string) => {
       await run('yarn', 'electron', '.');
       break;
     }
+    case 'release': {
+      await build(app);
+      // todo
+      break;
+    }
     default: {
       throw new Error(`Unknown command, ${command}`);
     }
   }
 };
+
+const apps = fs
+  .readdirSync(path.join(__dirname, '..', 'apps'))
+  .filter(name => !name.startsWith('.'));
+const commands = ['build', 'start', 'release'];
+const program = new Command();
+program
+  .option('-a --app <app>', 'app to manage')
+  .option('-c --command <command>', 'command to run');
+program.parse(process.argv);
+let {app, command} = program.opts();
+if (app && app.length > 2) {
+  app = apps.find(item => item.includes(app));
+}
+if (command && command.length > 2) {
+  command = commands.find(item => item.includes(command));
+}
 runCommand(command, app);
